387ec2fb436771e56516de1f2556b2ba23c26417
RolandMunsil <roland@munsil.com>
2015-01-24 18:28:18 -0800

    Actually change filename pt. 1
    Well Github is crashing when I try to change the name of shopKeeper.java
    to ShopKeeper.java, so I'm going to try a more roundabout method.

--- src/ShopKeeperasdf.java
+++ src/ShopKeeperasdf.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
-public class ShopKeeper{
+public class ShopKeeperasdf{
     private String name;
     private String type;
     private int level;
     private ArrayList<Item> items = new ArrayList<Item>();
     
-    public ShopKeeper(String name, String type, int level){
+    public ShopKeeperasdf(String name, String type, int level){
         this.name = name;
         this.type = type;
         this.level = level;
